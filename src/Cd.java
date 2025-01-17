4bce05d6bb0500c159a5768d3b5602d224c89cc3
Irove04 <maresctor4@alu.edu.gva.es>
2025-01-17 13:02:21 +0100

    Cd

---
+++ src/Cd.java
@@ -0,0 +1,14 @@
+public class Cd extends Almacenamiento{
+    public Cd(String nombre){
+        super(nombre);
+    }
+    public void leerDatos(){
+        System.out.println("Leo datos..");
+    }
+    public void escribirDatos(){
+        System.out.println("Escribo datos..");
+    }
+    public void reportarDatos(){
+        System.out.println("Reporto datos..");
+    }
+}
